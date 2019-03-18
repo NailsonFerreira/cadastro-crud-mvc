f99b190f1acd962ab76d96b14aa82669ef7da389
Nailson <nailson_ferreira@hotmail.com>
2019-03-18 08:44:49 -0300

    Antes da migration

---
+++ CadastroCrudMvc/Data/CadastroCrudMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroCrudMvc.Models
+{
+    public class CadastroCrudMvcContext : DbContext
+    {
+        public CadastroCrudMvcContext (DbContextOptions<CadastroCrudMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CadastroCrudMvc.Models.Department> Department { get; set; }
+    }
+}
